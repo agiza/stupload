0fb8bf4f107efe289fbf1bcc9fdc1db808057ba9
sanmugasundaram <k.sanmugam2@gmail.com>
2012-11-09 11:41:16 +0530

    ajax gif load inages addede

--- stupload.js
+++ stupload.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-	webroot = "http://10.102.152.101/spoken_tutorial_org/stupload/";
-	$('.uptn_foss_category_name').change(function(){
-		foss = $(this).val();
-		level = $('.uptn_tutorial_level').val();
+	webroot = "http://10.102.6.113/spoken_tutorial_org/stupload/";
+	$('.uptn_tutorial_level').change(function(){
+		foss = $('.uptn_foss_category_name').val();
+		level = $(this).val();
 		$.ajax({
 			type : 'POST',
 			url : webroot + "get_tutorial_levels",
 			data : {
 				'foss' : foss, 'level' : level
+			},
+			beforeSend: function() {
+			    field_data = $('.poll-form').html();
+			    $('.poll-form').html("<img src='http://10.102.6.113/spoken_tutorial_org/ajax-loader.gif' />");
 			},
 			success : function(data){
 				output = JSON.parse(data);
@@ -15,6 +19,7 @@ $(document).ready(function(){
 				console.log(output);
 				var html_data = "<option val=''>Order</option>";
 				if(output){
+					$('.poll-form').html(field_data);
 					for (var i=0; i < output.length; i++)
 					{
 						html_data += "<option value='"+ output[i] +"'>" + output[i] + "</option>";	
@@ -22,7 +27,7 @@ $(document).ready(function(){
 					console.log(html_data);
 					$('.uptn_tutorial_order_no').html(html_data);
 				}else{
-					
+					alert('Somthing wrong, Please refresh page');
 				}
 			}
 		});
