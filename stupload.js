83f0a5001260566d99ccf3569077f42c6b19115c
sanmugasundaram <k.sanmugam2@gmail.com>
2012-11-16 15:53:48 +0530

    english upload fnished

--- stupload.js
+++ stupload.js
@@ -32,8 +32,69 @@ $(document).ready(function(){
 			}
 		});
 	});
-	// adding the tutorial level
 	$('.upeng_foss_category_name').change(function(){
+		foss = $(this).val();
+		$.ajax({
+			type : 'POST',
+			url : webroot + "get_category_levels",
+			data : {
+				'foss' : foss
+			},
+			beforeSend: function() {
+			    field_data = $('.uenglish-level-name').html();
+			    $('.uenglish-level-name').html(loading_image);
+			},
+			success : function(data){
+				output = JSON.parse(data);
+				var html_data = "<option value=''>Level</option>";
+				if(output){
+					$('.uenglish-level-name').html(field_data);
+					for (var i=0; i < output.length; i++)
+					{
+						html_data += "<option value='"+ output[i].tutorial_level +"'>" + output[i].tutorial_level + "</option>";	
+					}
+					console.log(html_data);
+					$('.upeng_tutorial_level').html(html_data);
+					// adding the tutorial name under tutorial level
+					$('.upeng_tutorial_level').change(function(){
+						level = $(this).val();
+						$.ajax({
+							type : 'POST',
+							url : webroot + "get_category_names",
+							data : {
+								'level' : level
+							},
+							beforeSend: function() {
+							    field_data = $('.uenglish-name').html();
+							    $('.uenglish-name').html(loading_image);
+							},
+							success : function(data){
+								output = JSON.parse(data);
+								var html_data = "<option value=''>Tutorial name</option>";
+								if(output){
+									$('.uenglish-name').html(field_data);
+									for (var i=0; i < output.length; i++)
+									{
+										html_data += "<option value='"+ output[i].tutorial_name +"'>" + output[i].tutorial_name + "</option>";	
+									}
+									console.log(html_data);
+									$('.upeng_tutorial_name').html(html_data);
+								}else{
+									alert('Somthing wrong, Please refresh page');
+								}
+							}
+						});
+					});
+				}else{
+					alert('Somthing wrong, Please refresh page');
+				}
+			}
+		});
+	});
+
+
+	// adding the tutorial level
+	/*$('.upeng_foss_category_name').change(function(){
 		foss = $(this).val();
 		$.ajax({
 			type : 'POST',
@@ -91,5 +152,5 @@ $(document).ready(function(){
 				}
 			}
 		});
-	});
+	}); */
 });
